97c848657cdad086e6f24d543544877c7d257fc8
Glitch (pinnate-chatter-hiss) <none>
2021-07-24 09:34:29 +0000

    🍔📮 Checkpoint ./myApp.js:34175474/171

--- myApp.js
+++ myApp.js
@@ -26,6 +26,13 @@ app.get('/json', (req, res) => {
   }
 })
 
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next()
+}, (req, res) => {
+  res.send({time: req.time})
+})
+
 
 
 
